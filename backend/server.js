5d51fc8b438b14266c41067acb9a0edccbb75995
Khangdepzai <khangtgr@gmail.com>
2023-06-15 13:30:38 +0700

    Add auth protect middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
+import cookieParser from "cookie-parser";
+
 import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
 
 import userRoutes from "./routes/userRoutes.js";
@@ -17,6 +19,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
