be25787099617934b0e77715a5bfa1f3ef197a1d
Khangdepzai <khangtgr@gmail.com>
2023-06-15 11:40:21 +0700

    Initial commit - Update Database config

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
+
+import userRoutes from "./routes/userRoutes.js";
+
+import connectDB from "./configs/db.js";
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res, next) => res.send("Server is ready!"));
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, (message) => {
+  console.log(`Server started on port ${port}`);
+});
