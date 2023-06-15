23a440cbacee64c8365001964bc1842e65c10dd1
Khangdepzai <khangtgr@gmail.com>
2023-06-15 16:42:38 +0700

    Redux installation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import store from './store.js';
+import { Provider } from 'react-redux';
+
 import App from './App.jsx';
+
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from 'react-router-dom';
+
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import HomeScreen from './screens/HomeScreen.jsx';
 import LoginScreen from './screens/LoginScreen.jsx';
 import RegisterScreen from './screens/RegisterScreen.jsx';
@@ -24,7 +31,9 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
