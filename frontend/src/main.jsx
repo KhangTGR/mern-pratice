627b8f5b9c332f0411bff40c6ed432fa54c2e54d
Khangdepzai <khangtgr@gmail.com>
2023-06-22 12:19:24 +0700

    Finish Header

---
+++ frontend/src/main.jsx
@@ -0,0 +1,29 @@
+// Default imports
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+// Import React Router Dom
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+
+// Router definitions
+import Home from './pages/Home.jsx'
+import Menu from './pages/Menu.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index element={<Home />} />
+      <Route path='menu' element={<Menu />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+    </Route>
+  )
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <RouterProvider router={router} />
+)
